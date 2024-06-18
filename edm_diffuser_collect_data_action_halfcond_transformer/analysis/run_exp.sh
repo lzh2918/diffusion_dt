1a59b053b99fbd7d481e25ad6d6760c563b7c5a5
lzhplaying-104 <1725875704@qq.com>
2024-06-18 11:10:39 +0800

    edm 开启训练文件完成修改

--- edm_diffuser_collect_data_action_halfcond_transformer/analysis/run_exp.sh
+++ edm_diffuser_collect_data_action_halfcond_transformer/analysis/run_exp.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
-group="half_cond_diffusion"
-cuda=("2" "3" "4" "5" "6" "7")
+cuda=("2" "5" "4" "5" "6" "7")
+group="edm_diffusion"
 # group="test"
-task=("halfcheetah-medium-expert-v2" "hopper-medium-expert-v2" "walker2d-medium-expert-v2" "halfcheetah-medium-replay-v2" "hopper-medium-replay-v2" "walker2d-medium-replay-v2" )
-train_file="/home/liuzhihong/diffusion_related/diffusion_dt/decision_diffuser_collect_data_action_halfcond_transformer/analysis/train.py"
+task=("halfcheetah-medium-v2" "hopper-medium-v2" "walker2d-medium-v2" "halfcheetah-medium-expert-v2" "hopper-medium-expert-v2" "walker2d-medium-expert-v2")
+cond_length=(10 5 2) 
 horizon=20
-cond_length=(10 5 2)
+train_file="/home/liuzhihong/diffusion_related/diffusion_dt/code/edm_diffuser_collect_data_action_halfcond_transformer/analysis/train.py"
 
 
 $i
@@ -14,13 +13,17 @@ $j
 $log_index
 $cuda_index
 
-for ((i=0;i<=5;i++))
+for ((i=1;i<=1;i++))
 do
     for ((j=0;j<3;j++))
     do
     ((log_index = i*3+j))
-    log_dir="/home/liuzhihong/diffusion_related/diffusion_dt/exp_result/collect_data/log/half_cond_diffusion_$log_index.txt"
-    CUDA_VISIBLE_DEVICES=${cuda[$i]} python -u $train_file --group $group --task ${task[$i]} --cond_length ${cond_length[$j]} --horizon $horizon > $log_dir 2>&1 &
+    log_dir="/home/liuzhihong/diffusion_related/diffusion_dt/exp_result/log/collect_data/log/edm_diffusion_$log_index.txt"
+    CUDA_VISIBLE_DEVICES=${cuda[$i]} python -u $train_file \
+                                            --group $group \
+                                            --task ${task[$i]} \
+                                            --cond_length ${cond_length[$j]} \
+                                            --horizon $horizon > $log_dir 2>&1 &
     done
 sleep 2
 done
