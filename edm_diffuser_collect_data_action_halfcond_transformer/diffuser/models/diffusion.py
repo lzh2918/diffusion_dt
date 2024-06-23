10882184aaf584c60ad97cc19eaf2f04d0c20a1e
lzhplaying-104 <1725875704@qq.com>
2024-06-23 16:15:47 +0800

    part value diffusion

--- edm_diffuser_collect_data_action_halfcond_transformer/diffuser/models/diffusion.py
+++ edm_diffuser_collect_data_action_halfcond_transformer/diffuser/models/diffusion.py
@@ -460,6 +460,252 @@ class EDMDiffusion(nn.Module):
     def forward(self, cond, *args, **kwargs):
         # return self.conditional_sample(cond=cond, *args, **kwargs)
         return self.sample(cond, *args, **kwargs)
+    
+
+class EDMValueDiffusion(nn.Module):
+    """
+    main func: compute_loss sample
+    compute_loss{
+        1.与之前loss对接
+        2.维护内部各项函数与对应参数
+    }
+    sample{
+        1.输入与之前的sample对接
+        2.内部函数与参数
+    }
+    check norm{
+        1. 核心就是可以确保loss 和 sample 过程中这个norm的尺度是相同的。
+    }
+    edm_cfg={
+        num_steps_denoising: int
+        sigma_min: float = 2e-3
+        # simga sampler
+        sigma_max: float = 40
+        # diffusion sampler
+        sigma_max: float = 10
+        rho: int = 7
+        order: int = 1
+        s_churn: float = 0
+        s_tmin: float = 0
+        s_tmax: float = float("inf")
+        s_noise: float = 1
+        scale: float = 1.478
+        loc: float = -0.225
+        sigma_offset_noise: float = 1.0
+        sigma_data: float = 1.0
+        ·····
+    }
+    """
+    def __init__(self, model, horizon, observation_dim, action_dim, edm_cfg, n_timesteps=1000,
+        loss_type='l1', clip_denoised=False, predict_epsilon=True,
+        action_weight=1.0, loss_discount=1.0, loss_weights=None, returns_condition=False,
+        condition_guidance_w=0.1,):
+        super().__init__()
+        self.horizon = horizon
+        self.observation_dim = observation_dim
+        self.action_dim = action_dim
+        self.transition_dim = observation_dim + action_dim + 1 # 应为add了reward
+        self.returns_condition = returns_condition
+        self.condition_guidance_w = condition_guidance_w
+        # edm
+        self.inner_model = model
+        self.edm_cfg = edm_cfg
+        self.device = next(model.parameters()).device
+        # sigmas
+        self.sigmas = self.build_sigma(
+            denoise_steps=self.edm_cfg.num_steps_denoising,
+            sigma_min=self.edm_cfg.sigma_min,
+            sigma_max=self.edm_cfg.sigma_max_sampler,
+            rho=self.edm_cfg.rho,
+            device=self.device
+        )
+
+        betas = cosine_beta_schedule(n_timesteps)
+        alphas = 1. - betas
+        alphas_cumprod = torch.cumprod(alphas, axis=0)
+        alphas_cumprod_prev = torch.cat([torch.ones(1), alphas_cumprod[:-1]])
+
+        self.n_timesteps = int(n_timesteps)
+        self.clip_denoised = clip_denoised
+        self.predict_epsilon = predict_epsilon
+
+        self.register_buffer('betas', betas)
+        self.register_buffer('alphas_cumprod', alphas_cumprod)
+        self.register_buffer('alphas_cumprod_prev', alphas_cumprod_prev)
+
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+        self.register_buffer('sqrt_alphas_cumprod', torch.sqrt(alphas_cumprod))
+        self.register_buffer('sqrt_one_minus_alphas_cumprod', torch.sqrt(1. - alphas_cumprod))
+        self.register_buffer('log_one_minus_alphas_cumprod', torch.log(1. - alphas_cumprod))
+        self.register_buffer('sqrt_recip_alphas_cumprod', torch.sqrt(1. / alphas_cumprod))
+        self.register_buffer('sqrt_recipm1_alphas_cumprod', torch.sqrt(1. / alphas_cumprod - 1))
+
+        # calculations for posterior q(x_{t-1} | x_t, x_0)
+
+        
+        posterior_variance = betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
+        self.register_buffer('posterior_variance', posterior_variance)
+
+        ## log calculation clipped because the posterior variance
+        ## is 0 at the beginning of the diffusion chain
+        self.register_buffer('posterior_log_variance_clipped',
+            torch.log(torch.clamp(posterior_variance, min=1e-20)))
+        self.register_buffer('posterior_mean_coef1',
+            betas * np.sqrt(alphas_cumprod_prev) / (1. - alphas_cumprod))
+        self.register_buffer('posterior_mean_coef2',
+            (1. - alphas_cumprod_prev) * np.sqrt(alphas) / (1. - alphas_cumprod))
+
+        ## get loss coefficients and initialize objective
+        loss_weights = self.get_loss_weights(action_weight, loss_discount, loss_weights)
+        self.loss_fn = Losses[loss_type](loss_weights, self.action_dim)
+
+    def get_loss_weights(self, action_weight, discount, weights_dict):
+        '''
+            sets loss coefficients for trajectory
+
+            action_weight   : float
+                coefficient on first action loss
+            discount   : float
+                multiplies t^th timestep of trajectory loss by discount**t
+            weights_dict    : dict
+                { i: c } multiplies dimension i of observation loss by c
+        '''
+        self.action_weight = action_weight
+
+        dim_weights = torch.ones(self.transition_dim, dtype=torch.float32)
+
+        ## set loss coefficients for dimensions of observation
+        if weights_dict is None: weights_dict = {}
+        for ind, w in weights_dict.items():
+            dim_weights[self.action_dim + ind] *= w
+
+        ## decay loss with trajectory timestep: discount**t
+        discounts = discount ** torch.arange(self.horizon, dtype=torch.float)
+        discounts = discounts / discounts.mean()
+        loss_weights = torch.einsum('h,t->ht', discounts, dim_weights)
+
+        ## manually set a0 weight
+        loss_weights[0, :self.action_dim] = action_weight
+        return loss_weights
+    
+    #------------------------------------------ edm_compounent ------------------------------------------#
+    def sample_sigma_training(self, batch_size, device):
+        s = torch.randn(batch_size, device=device) * self.edm_cfg.scale + self.edm_cfg.loc
+        return s.exp().clip(self.edm_cfg.sigma_min, self.edm_cfg.sigma_max_training)
+    
+    def compute_edm_coef(self, sigma):
+        sigma = (sigma**2 + self.edm_cfg.sigma_offset_noise**2).sqrt()
+        c_in = 1 / (sigma**2 + self.edm_cfg.sigma_data**2).sqrt()
+        c_skip = self.edm_cfg.sigma_data**2 / (sigma**2 + self.edm_cfg.sigma_data**2)
+        c_out = sigma * c_skip.sqrt()
+        c_noise = sigma.log() / 4
+        return *(add_dims(c, 3) for c in (c_in, c_out, c_skip)), add_dims(c_noise, 1)
+    
+    def denoise_forward(self, noisy_x, sigma, cond_traj, returns):
+        c_in, c_out, c_skip, c_noise = self.compute_edm_coef(sigma)
+        
+        # rescale
+        rescale_cond = cond_traj / self.edm_cfg.sigma_data
+        rescale_noise = noisy_x * c_in
+
+        # add cond traj (直接cat edm是这样的,但是transformer mask要做好,不然会有点问题)
+        cond_length = cond_traj.shape[1]
+        rescale_noise = torch.cat([rescale_cond, rescale_noise], dim=1) # shape check
+
+        # model output (returns的格式要注意保持一致)
+        out_seq_length = rescale_noise.shape[1] - cond_length
+        model_output = self.inner_model(rescale_noise, c_noise, returns)[:,-out_seq_length:]
+
+        # denoised (按理来说 这个是预测的后半段轨迹)
+        denoised = model_output * c_out + noisy_x * c_skip
+        return model_output, denoised
+
+
+    def compute_loss(self, x, cond, returns=None):
+        """
+        x: [batch_size, seq_len, traj_dim]
+        cond[0]: [batch_size, cond_length, traj_dim]
+        return: [batch_size]
+        """
+        cond_length = cond[0].shape[1]
+        batch_size, seq_len = x.shape[0], x.shape[1]
+        device = x.device
+
+        # reshape 因为model预测的是后面那一段，所以进行一个reshape
+        x = x[:, cond_length:]
+        seq_len = seq_len - cond_length
+
+        #加噪 
+        sigma = self.sample_sigma_training(batch_size, device)
+        _, c_out, c_skip, _ = self.compute_edm_coef(sigma)
+        offset_noise = self.edm_cfg.sigma_offset_noise * torch.randn(batch_size, seq_len, 1, device=device)
+        noisy_x = x + offset_noise + torch.randn_like(x) * add_dims(sigma, x.ndim) # 等，这个加完噪之后，噪声强度很强的之后肯定要有归一化
+
+        # model output
+        model_output, denoised = self.denoise_forward(noisy_x, sigma, cond[0], returns)
+
+        # compute loss
+        target = (x - c_skip * noisy_x) / c_out
+        loss = F.mse_loss(model_output, target)
+
+        return loss, {"loss_denoising": loss.detach()}
+    
+    def build_sigma(self, denoise_steps, sigma_min, sigma_max, rho, device):
+        min_inv_rho = sigma_min ** (1 / rho)
+        max_inv_rho = sigma_max ** (1 / rho)
+        l = torch.linspace(0, 1, denoise_steps, device=device)
+        sigmas = (max_inv_rho + l * (min_inv_rho - max_inv_rho)) ** rho
+        return torch.cat((sigmas, sigmas.new_zeros(1)))
+
+
+    @torch.no_grad()
+    def sample(self, cond, returns=None, horizon=None, *args, **kwargs):
+        # 参数准备
+        device = cond[0].device
+        batch_size, cond_length, traj_dim = cond[0].shape[0], cond[0].shape[1], cond[0].shape[2]
+        predict_seq_len = self.horizon - cond_length
+        s_in = torch.ones(batch_size, device=device)
+        gamma_ = min(self.edm_cfg.s_churn / (len(self.sigmas) - 1), 2**0.5 - 1)
+        
+        # denoise 可能是只要均值一样
+        x = torch.randn(batch_size, predict_seq_len, traj_dim, device=device)
+
+        for sigma, next_sigma in zip(self.sigmas[:-1], self.sigmas[1:]):
+            gamma = gamma_ if self.edm_cfg.s_tmin <= sigma <= self.edm_cfg.s_tmax else 0
+            sigma_hat = sigma * (gamma + 1)
+            if gamma > 0:
+                eps = torch.randn_like(x) * self.edm_cfg.s_noise
+                x = x + eps * (sigma_hat**2 - sigma**2) ** 0.5
+
+            _, denoised = self.denoise_forward(x, sigma * s_in, cond[0], returns) # 归一化有没有复原？
+
+            d = (x - denoised) / sigma_hat
+            dt = next_sigma - sigma_hat
+            if self.edm_cfg.order == 1 or next_sigma == 0:
+                # Euler method
+                x = x + d * dt
+            else:
+                # Heun's method
+                x_2 = x + d * dt
+                _, denoised_2 = self.denoise_forward(x_2, next_sigma * s_in, cond, returns)
+                d_2 = (x_2 - denoised_2) / next_sigma
+                d_prime = (d + d_2) / 2
+                x = x + d_prime * dt
+        
+        # 进行拼接
+        total_traj = torch.cat([cond[0], x], dim=1) # 检查归一化是否一致。
+
+        return total_traj
+
+    def loss(self, x, cond, returns=None):
+        # batch_size = len(x)
+        # t = torch.randint(0, self.n_timesteps, (batch_size,), device=x.device).long()
+        # return self.p_losses(x, cond, t, returns)
+        return self.compute_loss(x,cond,returns)
+
+    def forward(self, cond, *args, **kwargs):
+        # return self.conditional_sample(cond=cond, *args, **kwargs)
+        return self.sample(cond, *args, **kwargs)
 
 
 class GaussianDiffusion(nn.Module):
